c3d55f229cebd0e1d5b70ae8f03ab449f54adbaf
02suheila <84657568+02suheila@users.noreply.github.com>
2021-07-06 16:19:19 +0530

    loop.java

---
+++ loop.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if(n%2 == 1 || n>= 6 && n<=20){
+         System.out.println(weird);   
+        }
+        else if (n%2 == 0 || n >= 2 && n <= 5){
+           System.out.println(not );   
+        }
+        else
+        
+           System.out.println(not );   
+        
+    }
+}
+        
+       
